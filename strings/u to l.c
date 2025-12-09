6592f09e0830ceb4384136d9c3963ed6e2d96f72
vamshithavempati <vamshithavempati02@gmail.com>
2025-12-09 18:14:33 +0530

    Create u to l.c

---
+++ strings/u to l.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char st[30];
+    int i;
+    printf("Enter the text:\n");
+    gets(st);
+    for(i = 0; st[i] != '\0'; i++)
+    {
+        if(st[i] >= 'A' && st[i] <= 'Z')
+        {
+            st[i] = st[i] + 32;
+        }
+    }
+    puts(st);
+    return 0;
+}
