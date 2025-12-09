27d324c94596995f5761dafcbd64a0ecd1a87bf6
vamshithavempati <vamshithavempati02@gmail.com>
2025-12-09 19:05:26 +0530

    Create reversefun.c

---
+++ strings/reversefun.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int reverse(int n);   
+int main()
+{
+    int num, rev;
+    printf("Enter a number:\n");
+    scanf("%d", &num);
+    rev = reverse(num);  
+    printf("Reverse of the number = %d\n", rev);
+    return 0;
+}
+
+int reverse(int n)
+{
+    int r, rev = 0;
+
+    while(n > 0)
+    {
+        r = n % 10;            
+ rev = rev * 10 + r;    
+        n = n / 10;          
+    }
+    return rev;
+}
